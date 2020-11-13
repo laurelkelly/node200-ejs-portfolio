5059c5a3bc05180f131c92ea8a67dbefe79fb999
Laurel Kelly <laurellkelly@gmail.com>
2020-11-13 08:38:35 -0800

    Update index.js with correct PORT

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const server = require('./app');
 
 // listen for incoming connections
-server.listen(3000, () => {
-    console.log('Server is listening at http://localhost:3000');
-});
+
+// server.listen(3000, () => {
+//     console.log('Server is listening at http://localhost:3000');
+// });
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
